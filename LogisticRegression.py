be9cc935e2eb9a3cf1eb0ceda784b8dcca36895f
stevennn <1136671955@qq.com>
2019-04-10 14:46:25 +0800

    Initial commit

---
+++ LogisticRegression.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+'''
+        司马懿：“善败能忍，然厚积薄发”
+                                    ——李叔说的
+code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+          --┃      ☃      ┃--
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗II━II┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+ @Belong = 'LogisticRegression'  @MadeBy = 'PyCharm'
+ @Author = 'steven'   @DateTime = '2019/4/10 14:43'
+'''
+import numpy as np
+class logisticRegression:
+    def __init__(self,inputSize):
+        self.inputSize=inputSize
+        self.w=np.ra
