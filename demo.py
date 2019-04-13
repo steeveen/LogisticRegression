5bfb5ac5b3e79598db89616e95b9af76be18ae7a
stevennn <1136671955@qq.com>
2019-04-13 14:19:07 +0800

    增加了初始化分类器时，指定学习率的功能 （接上一次commit） 权重更新时的平均由self.input改为x的样本数量

--- demo.py
+++ demo.py
@@ -24,15 +24,15 @@ from LogisticRegression import LogisticRegression
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
-data=load_iris()
-x=data['data']
-y=(data['target']>0).astype(np.int8)
-trainX,testX,trainY,testY=train_test_split(x,y,test_size=0.2,shuffle=True)
-trainX=trainX.T
-testX=testX.T
-trainY=trainY.reshape(1,-1)
-testY=testY.reshape(1,-1)
-featureNum=4
-lg=LogisticRegression(featureNum,{'lr':0.00001})
-lg.fit(trainX,trainY,2000)
-lg.eva(testX,testY)
+data = load_iris()
+x = data['data']
+y = (data['target'] > 0).astype(np.int8)
+trainX, testX, trainY, testY = train_test_split(x, y, test_size=0.2)
+trainX = trainX.T
+testX = testX.T
+trainY = trainY.reshape(1, -1)
+testY = testY.reshape(1, -1)
+featureNum = 4
+lg = LogisticRegression(featureNum, {'lr': 0.00001})
+lg.fit(trainX, trainY, 2000)
+lg.eva(testX, testY)
